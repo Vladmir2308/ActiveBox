e8dda0a236a2224ae25c0bdc2bf2bebb71eb76d1
Vladmir2308 <vladimir.kanarsky@yandex.ru>
2023-01-15 10:54:10 +0300

    add smooth scroll, toggle, fixed header

---
+++ app.js
@@ -0,0 +1,49 @@
+$(function(){
+    let header = $('#header');
+    let introH = $('#intro').innerHeight();
+    /* Fixed Header */
+    $(window).on('scroll resize load', function(){
+        let scroll = $(this).scrollTop();
+
+        if (scroll > introH - 170){
+            header.addClass('fixed');
+            $('#nav').addClass('fixed');
+        }
+        else{
+            header.removeClass('fixed');
+            $('#nav').removeClass('fixed');
+        }
+    });
+
+    /* Smooth scroll */
+    $('[data-scroll]').on('click', function(event){
+        event.preventDefault();
+
+        let dataItem = $(this).data('scroll');
+        let scrollOffset = $(dataItem).offset().top;
+
+        console.log(scrollOffset);
+
+        $('html, body').animate({
+            scrollTop: scrollOffset - 90
+        }, 1000);
+
+        $('#toggle').removeClass('active');
+        $('#nav').removeClass('nav--toggle');
+    });
+
+    /* Toggle */
+    $('#toggle').on('click', function(event){
+        event.preventDefault();
+
+        $(this).toggleClass('active');
+        $('#nav').toggleClass('nav--toggle');
+    });
+
+    /* Slider */
+    $('[data-slider]').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
